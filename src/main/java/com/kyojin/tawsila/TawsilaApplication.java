095d042b96bb496009e7022487ee3b798ff915f0
tahajaiti <taha.jaiti@gmail.com>
2025-10-20 19:45:34 +0100

    FIX: fixed issues with configuration and .env

--- src/main/java/com/kyojin/tawsila/TawsilaApplication.java
+++ src/main/java/com/kyojin/tawsila/TawsilaApplication.java
@@ -2,8 +2,17 @@ package com.kyojin.tawsila;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                JpaRepositoriesAutoConfiguration.class
+        }
+)
+@ImportResource("classpath:applicationContext.xml")
 public class TawsilaApplication {
 
     public static void main(String[] args) {
