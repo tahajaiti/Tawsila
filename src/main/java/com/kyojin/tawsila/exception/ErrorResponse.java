9910fa0c50a40dd1febb5fd94f7363e990ce8ed0
tahajaiti <taha.jaiti@gmail.com>
2025-10-20 16:05:20 +0100

    FEAT: TS-14 added the error response class

---
+++ src/main/java/com/kyojin/tawsila/exception/ErrorResponse.java
@@ -0,0 +1,25 @@
+package com.kyojin.tawsila.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private String traceId;
+    private List<String> validationErrors;
+
+
+}
