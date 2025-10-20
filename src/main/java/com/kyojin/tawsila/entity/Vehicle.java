07cc6a25489cb793a2ba3f0f99621faac7c3462a
tahajaiti <taha.jaiti@gmail.com>
2025-10-20 16:48:50 +0100

    FEAT: TS-16 created the vehicule entity

---
+++ src/main/java/com/kyojin/tawsila/entity/Vehicle.java
@@ -0,0 +1,41 @@
+package com.kyojin.tawsila.entity;
+
+import com.kyojin.tawsila.enums.VehicleType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "vehicles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private VehicleType type;
+
+    @Column(nullable = false)
+    private double maxWeightKg;
+
+    @Column(nullable = false)
+    private double maxVolumeM3;
+
+    @Column(nullable = false)
+    private int maxDeliveries;
+
+    public Vehicle(VehicleType type) {
+        this.maxWeightKg = type.getMaxWeightKg();
+        this.maxVolumeM3 = type.getMaxVolumeM3();
+        this.maxDeliveries = type.getMaxDeliveries();
+    }
+
+}
