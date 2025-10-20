b9cf07d8608c865bcf19aed8796cb5f1a5438fc8
tahajaiti <taha.jaiti@gmail.com>
2025-10-20 12:25:47 +0100

    INIT: TS-9 initial files for the project

---
+++ src/main/java/com/kyojin/tawsila/TawsilaApplication.java
@@ -0,0 +1,13 @@
+package com.kyojin.tawsila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawsilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TawsilaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyojin/tawsila/TawsilaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyojin.tawsila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawsilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
