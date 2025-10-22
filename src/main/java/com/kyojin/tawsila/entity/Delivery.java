c97a8fb8d93eecaade30cdeacae03a4e8d872156
tahajaiti <taha.jaiti@gmail.com>
2025-10-22 10:15:24 +0100

    FEAT: TS-23 added validations for null values in the delivery entity

--- src/main/java/com/kyojin/tawsila/entity/Delivery.java
+++ src/main/java/com/kyojin/tawsila/entity/Delivery.java
@@ -19,21 +19,27 @@ public class Delivery {
     private Long id;
 
     @NotNull
+    @Column(nullable = false)
     private Double latitude;
 
     @NotNull
+    @Column(nullable = false)
     private Double longitude;
 
     @NotNull
+    @Column(name = "weight_kg", nullable = false)
     private Double weightKg;
 
     @NotNull
+    @Column(name = "volume_m3", nullable = false)
     private Double volumeM3;
 
+    @Column(name = "time_slot")
     private String timeSlot; // ex: "09:00-12:00"
 
-    @Enumerated(EnumType.STRING)
     @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private DeliveryStatus status;
 
     @PrePersist
