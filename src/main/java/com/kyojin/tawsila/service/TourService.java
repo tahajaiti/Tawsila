95be92553106403b50d03296c5ffd837a2a960ac
tahajaiti <taha.jaiti@gmail.com>
2025-10-24 16:43:40 +0100

    FEAT: TS-40 added the crud methods for tour annonated with jdoc

--- src/main/java/com/kyojin/tawsila/service/TourService.java
+++ src/main/java/com/kyojin/tawsila/service/TourService.java
@@ -1,10 +1,47 @@
 package com.kyojin.tawsila.service;
 
 import com.kyojin.tawsila.dto.DeliveryDTO;
+import com.kyojin.tawsila.dto.TourDTO;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TourService {
+
+    /**
+     * Create a new tour
+     * @param dto Data transfer object containing tour details
+     * @return Created tour DTO
+     */
+    TourDTO createTour(TourDTO dto);
+
+    /**
+     * Retrieve a tour by its ID
+     * @param id ID of the tour
+     * @return Optional containing the tour DTO if found, empty otherwise
+     */
+    Optional<TourDTO> getTourById(Long id);
+
+    /**
+     * Retrieve all tours
+     * @return List of all tour DTOs
+     */
+    List<TourDTO> getAllTours();
+
+    /**
+     * Update an existing tour
+     * @param id ID of the tour to update
+     * @param dto Data transfer object containing updated tour details
+     * @return Updated tour DTO
+     */
+    TourDTO updateTour(Long id, TourDTO dto);
+
+    /**
+     * Delete a tour by its ID
+     * @param id ID of the tour to delete
+     */
+    void deleteTour(Long id);
+
 
     /**
      * Get optimized tour using specified algorithm
---
+++ src/main/java/com/kyojin/tawsila/service/impl/TourServiceImpl.java
@@ -0,0 +1,10 @@
+package com.kyojin.tawsila.service.impl;
+
+import com.kyojin.tawsila.service.TourService;
+
+public class TourServiceImpl implements TourService {
+
+
+
+
+}
