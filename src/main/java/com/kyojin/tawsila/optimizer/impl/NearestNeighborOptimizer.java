a3d6ccb6a4b4fdc84f4d2f7b3b58047a8e0bd99d
tahajaiti <taha.jaiti@gmail.com>
2025-10-24 17:28:00 +0100

    FEAT: TS-51 implemnted the nearestneighbour logic for calculating the optimized tour

--- src/main/java/com/kyojin/tawsila/optimizer/impl/NearestNeighborOptimizer.java
+++ src/main/java/com/kyojin/tawsila/optimizer/impl/NearestNeighborOptimizer.java
@@ -1,7 +1,57 @@
 package com.kyojin.tawsila.optimizer.impl;
 
+import com.kyojin.tawsila.entity.Delivery;
+import com.kyojin.tawsila.entity.Warehouse;
 import com.kyojin.tawsila.optimizer.TourOptimizer;
+import com.kyojin.tawsila.util.DistanceCalculator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NearestNeighborOptimizer implements TourOptimizer {
-    
+    @Override
+    public List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries) {
+        if (deliveries == null || deliveries.isEmpty()) return new ArrayList<>();
+
+        // two lists to keep track of optimized and not visited deliveries
+        List<Delivery> optimized = new ArrayList<>();
+        List<Delivery> notVisited = new ArrayList<>(deliveries);
+
+        // we start from the warehouse position
+        double currentLat = warehouse.getLatitude();
+        double currentLon = warehouse.getLongitude();
+
+        while (!notVisited.isEmpty()) {
+            Delivery nearestDelivery = notVisited.get(0);
+
+            // we get the minimum distance delivery from the current position
+            double minDistance = DistanceCalculator.calculateDistance(
+                    currentLat, currentLon,
+                    nearestDelivery.getLatitude(), nearestDelivery.getLongitude()
+            );
+
+            for (Delivery delivery: notVisited) {
+                // calculate distance from current position to delivery
+                double dist = DistanceCalculator.calculateDistance(
+                        currentLat, currentLon,
+                        delivery.getLatitude(), delivery.getLongitude()
+                );
+
+                // update nearest delivery if found a closer one
+                if (dist < minDistance) {
+                    minDistance = dist;
+                    nearestDelivery = delivery;
+                }
+            }
+
+            optimized.add(nearestDelivery);
+            notVisited.remove(nearestDelivery);
+            
+            // update current position to the last added delivery
+            currentLat = nearestDelivery.getLatitude();
+            currentLon = nearestDelivery.getLongitude();
+        }
+
+        return optimized;
+    }
 }
