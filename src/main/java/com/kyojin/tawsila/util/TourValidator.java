3145764b215b61c7d06a90d534b5af8280904358
tahajaiti <taha.jaiti@gmail.com>
2025-10-25 16:27:58 +0100

    FEAT: TS-44 improved the tour validator to use the handle method inside the enum

--- src/main/java/com/kyojin/tawsila/util/TourValidator.java
+++ src/main/java/com/kyojin/tawsila/util/TourValidator.java
@@ -2,6 +2,7 @@ package com.kyojin.tawsila.util;
 
 import com.kyojin.tawsila.entity.Delivery;
 import com.kyojin.tawsila.entity.Tour;
+import com.kyojin.tawsila.enums.VehicleType;
 import com.kyojin.tawsila.exception.CapacityExceededException;
 import com.kyojin.tawsila.exception.MaxDeliveriesExceededException;
 
@@ -25,19 +26,39 @@ public class TourValidator {
 
         int totalDeliveries = deliveries.size();
 
-        if (totalWeight > vehicle.getMaxWeightKg()) {
-            throw new CapacityExceededException("Total weight of deliveries " + totalWeight +
-                    " kg exceeds vehicle's maximum weight capacity " + vehicle.getMaxWeightKg() + " kg.");
-        }
+//        if (totalWeight > vehicle.getMaxWeightKg()) {
+//            throw new CapacityExceededException("Total weight of deliveries " + totalWeight +
+//                    " kg exceeds vehicle's maximum weight capacity " + vehicle.getMaxWeightKg() + " kg.");
+//        }
+//
+//        if (totalVolume > vehicle.getMaxVolumeM3()) {
+//            throw new CapacityExceededException("Total volume of deliveries " + totalVolume +
+//                    " m3 exceeds vehicle's maximum volume capacity " + vehicle.getMaxVolumeM3() + " m3.");
+//        }
+//
+//        if (totalDeliveries > vehicle.getMaxDeliveries()) {
+//            throw new MaxDeliveriesExceededException("Total number of deliveries " + totalDeliveries +
+//                    " exceeds vehicle's maximum delivery count " + vehicle.getMaxDeliveries() + ".");
+//        }
 
-        if (totalVolume > vehicle.getMaxVolumeM3()) {
-            throw new CapacityExceededException("Total volume of deliveries " + totalVolume +
-                    " m3 exceeds vehicle's maximum volume capacity " + vehicle.getMaxVolumeM3() + " m3.");
-        }
 
-        if (totalDeliveries > vehicle.getMaxDeliveries()) {
-            throw new MaxDeliveriesExceededException("Total number of deliveries " + totalDeliveries +
-                    " exceeds vehicle's maximum delivery count " + vehicle.getMaxDeliveries() + ".");
+        VehicleType type = vehicle.getType();
+
+        if (!type.canHandle(totalWeight, totalVolume, totalDeliveries)) {
+            if (totalWeight > type.getMaxWeightKg()) {
+                throw new CapacityExceededException("Total weight " + totalWeight +
+                        " kg exceeds " + type + " max of " + type.getMaxWeightKg() + " kg.");
+            }
+
+            if (totalVolume > type.getMaxVolumeM3()) {
+                throw new CapacityExceededException("Total volume " + totalVolume +
+                        " m3 exceeds " + type + " max of " + type.getMaxVolumeM3() + " m3.");
+            }
+
+            if (totalDeliveries > type.getMaxDeliveries()) {
+                throw new MaxDeliveriesExceededException("Total deliveries " + totalDeliveries +
+                        " exceeds " + type + " max of " + type.getMaxDeliveries() + ".");
+            }
         }
     }
 }
