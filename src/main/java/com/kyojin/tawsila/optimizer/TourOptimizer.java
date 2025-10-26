7883a99b177c8bd5d317c168338273577e05387f
tahajaiti <taha.jaiti@gmail.com>
2025-10-26 21:05:52 +0100

    FEAT: TS-49 updated the touroptimizer interface added javadocs and updated the signature

--- src/main/java/com/kyojin/tawsila/optimizer/TourOptimizer.java
+++ src/main/java/com/kyojin/tawsila/optimizer/TourOptimizer.java
@@ -1,10 +1,20 @@
 package com.kyojin.tawsila.optimizer;
 
 import com.kyojin.tawsila.entity.Delivery;
+import com.kyojin.tawsila.entity.Vehicle;
 import com.kyojin.tawsila.entity.Warehouse;
 
 import java.util.List;
 
 public interface TourOptimizer {
-    List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries);
+
+    /**
+     * Calculates an optimized list of deliveries for a *single vehicle*.
+     *
+     * @param warehouse The starting/ending point (depot).
+     * @param deliveries The list of all deliveries to be considered.
+     * @param vehicle    The specific vehicle that will perform this tour.
+     * @return An ordered list of deliveries that respects the vehicle's constraints.
+     */
+    List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries, Vehicle vehicle);
 }
