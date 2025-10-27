410d73fd966786cb48773bfd4ff258a64a9b952a
tahajaiti <taha.jaiti@gmail.com>
2025-10-27 17:44:15 +0100

    FEAT: TS-67 created the tour controller class

---
+++ src/main/java/com/kyojin/tawsila/controller/TourController.java
@@ -0,0 +1,9 @@
+package com.kyojin.tawsila.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tours")
+public class TourController {
+}
