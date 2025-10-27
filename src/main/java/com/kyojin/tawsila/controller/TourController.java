816cf2172a55da00f2204eaeabbd17dffc350db0
tahajaiti <taha.jaiti@gmail.com>
2025-10-27 17:50:41 +0100

    FEAT: TS-67 created all the main crud endpoints for the tour controller

--- src/main/java/com/kyojin/tawsila/controller/TourController.java
+++ src/main/java/com/kyojin/tawsila/controller/TourController.java
@@ -1,9 +1,52 @@
 package com.kyojin.tawsila.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kyojin.tawsila.dto.TourDTO;
+import com.kyojin.tawsila.entity.Tour;
+import com.kyojin.tawsila.service.TourService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tours")
 public class TourController {
+
+    private final TourService tourService;
+
+    public TourController(TourService tourService) {
+        this.tourService = tourService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TourDTO> createTour(@Valid @RequestBody TourDTO tourDTO) {
+        var createdTour = tourService.createTour(tourDTO);
+        return ResponseEntity.ok(createdTour);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TourDTO>> getAllTours() {
+        return  ResponseEntity.ok(tourService.getAllTours());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TourDTO> getTour(@PathVariable Long id) {
+        return tourService.getTourById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TourDTO> updateTour(@PathVariable Long id, @Valid @RequestBody TourDTO tourDTO) {
+        var updatedTour = tourService.updateTour(id, tourDTO);
+        return ResponseEntity.ok(updatedTour);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTour(@PathVariable Long id) {
+        tourService.deleteTour(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
